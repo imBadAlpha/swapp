a53b957eb7acddf3b701d5a1ccd1c9ecdf907fa8
IamBadAlpha <romeonombrejr@gmail.com>
2023-01-29 13:31:39 +0800

    Route middleware changes for '/profile'

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'user'])->name('dashboard');
 
-Route::middleware(['auth', 'user'])->group(function () {
+Route::middleware(['auth', 'verified', 'user'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
