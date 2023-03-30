3e6392709f9218ecee2502be0f900e2ce94a7929
IamBadAlpha <romeonombrejr@gmail.com>
2023-03-30 18:28:11 +0800

    Removed READ.md

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
+    use Searchable;
+
+    public function searchableAs()
+    {
+        return 'posts_index';
+    }
+
+    public function toSearchableArray()
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+        ];
+    }
+
     /**
      * The attributes that are mass assignable.
      *
